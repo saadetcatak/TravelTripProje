bc1326d083fdf341168d4aa279a7620db18e4490
saadetcatakakgunes1211 <saadetcatak1453@gmail.com>
2023-05-25 22:53:56 +0300

    Admin Paneli

---
+++ TravelTripProje/Controllers/AdminController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
+namespace TravelTripProje.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        Context c= new Context();
+        public ActionResult Index()
+        {
+            var degerler =c.Blogs.ToList();
+            return View(degerler);
+        }
+
+        [HttpGet]
+        public ActionResult YeniBlog()
+        { 
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult YeniBlog(Blog p) 
+
+        { 
+            c.Blogs.Add(p);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult BlogSil(int id)
+        {
+            var values = c.Blogs.Find(id);
+            c.Blogs.Remove(values);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult BlogGetir(int id) 
+        {
+            var values = c.Blogs.Find(id);
+            return View("BlogGetir",values);
+        }
+    }
+}
